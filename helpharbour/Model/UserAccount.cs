6173dfbf053966ec3c3ebf251274922d05cb8387
Faraz Amjad <fa152@students.waikato.ac.nz>
2024-04-01 16:27:10 +1300

    Created Model file for user_account collection.

---
+++ helpharbour/Model/UserAccount.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace helpharbour.Model
+{
+    [BsonIgnoreExtraElements]
+    public class UserAccount
+    {
+        [BsonElement("userID")]
+        public int userID { get; set; }
+
+        [BsonElement("username")]
+        public string username { get; set; }
+
+        [BsonElement("password")]
+        public string password { get; set; }
+
+        [BsonElement("role")]
+        public string role { get; set; }
+
+        [BsonElement("groupName")]
+        public string groupName { get; set; }
+       
+    }
+}
